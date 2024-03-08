f9a300db77f066cddf57a7eca6b40b170f4cddef
Hubert Tułacz <htulacz@student.agh.edu.pl>
2024-03-08 16:57:24 +0100

    lab2 pocz

---
+++ zad2/collatz.c
@@ -0,0 +1,15 @@
+int collatz_conjecture(int input){
+    if (input % 2 == 0){
+        return input / 2;
+    }else{
+        return input * 3 + 1;
+    }
+}
+int test_collatz_convergence(int input, int max_iter){
+    int n = input;
+    int i = 0;
+    while(n != 1 && i <= max_iter){
+        n = collatz_conjecture(n);
+    }
+    return (i < max_iter) ? (i) : (-1);
+}
