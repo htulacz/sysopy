6e30e517cfe72005e7ca15ea2c72cc29676e02e7
Hubert Tułacz <htulacz@student.agh.edu.pl>
2024-04-04 15:22:58 +0200

    lab4 rdyy

--- lab4/zad2/zad2.c
+++ lab4/zad2/zad2.c
@@ -4,6 +4,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int global = 0;
+
+
 int main(int argc, char * argv[]){
-    
+    int local = 0;
+    pid_t pid = fork();
+
+    if (pid == 0){
+        printf("childs process\n");
+        global ++;
+        local ++;
+        printf("child pid = %d, parent pid = %d\n", getpid(), getppid());
+        printf("child's local = %d, child's global = %d\n", local, global);
+
+        execl("/bin/ls", "ls", argv[1], NULL);
+        perror("Błąd execl");
+        return EXIT_FAILURE;
+    } else {
+        wait(NULL);
+        printf("parent process\n");
+        printf("parent pid = %d, child pid = %d\n", getpid(), pid);
+
+        int status;
+        wait(&status); // czekaj na zakończenie procesu potomnego i odczytaj status
+
+        if (WIFEXITED(status)) {
+            printf("Child exit code: %d\n", WEXITSTATUS(status));
+        }
+
+        printf("Parent's local = %d, parent's global = %d\n", local, global);
+
+        return EXIT_SUCCESS;
+    }
 }
